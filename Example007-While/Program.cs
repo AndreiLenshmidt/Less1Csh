5364498e44749169d4f53c7525e47f15367ed4ed
LenshmidtAndrei <lenshmidt.andrei@gmail.com>
2022-10-12 00:12:17 +0300

    Example007 final

--- Example007-While/Program.cs
+++ Example007-While/Program.cs
@@ -2,12 +2,7 @@
 //Console.SetCursorPosition(10,4);
 //Console.WriteLine("+");
 
-int xa = 1; 
-int ya = 1;
-int xb = 1; 
-int yb = 30;
-int xc = 40; 
-int yc = 30;
+int xa = 1, ya = 1, xb = 1, yb = 30, xc = 60, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
